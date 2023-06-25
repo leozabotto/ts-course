79b0312ce24d22a1ffb524f933e48b557d35749d
Leonardo Zabotto <leozabotto@outlook.com>
2023-06-25 15:02:27 -0300

    ISP + LSP

---
+++ 17 - TypeScript/ts-solid/src/isp/main.ts
@@ -0,0 +1,45 @@
+/*
+Interface segregation principle (Princípio da segregação de Interface) -
+os clientes não devem ser forçados a depender de types, interfaces ou membros
+abstratos que não utilizam
+*/
+import { ShoppingCart } from './classes/shopping-cart';
+import { Order } from './classes/order';
+import { Messaging } from './services/messaging';
+import { Persistency } from './services/persistency';
+import { Product } from './classes/product';
+import { NoDiscount } from './classes/discount';
+import { EnterpriseCustomer } from './classes/customer';
+
+// const fiftyPercentDiscount = new FiftyPercentDiscount();
+// const tenPercentDiscount = new TenPercentDiscount();
+const noDiscount = new NoDiscount();
+const shoppingCart = new ShoppingCart(noDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+// const individualCustomer = new IndividualCustomer(
+//   'Luiz',
+//   'Miranda',
+//   '111.111.111-11',
+// );
+const enterpriseCustomer = new EnterpriseCustomer(
+  'Empresa Gigante',
+  '2222222222222222',
+);
+const order = new Order(
+  shoppingCart,
+  messaging,
+  persistency,
+  enterpriseCustomer,
+);
+
+shoppingCart.addItem(new Product('Camiseta', 49.91));
+shoppingCart.addItem(new Product('Caderno', 9.9123));
+shoppingCart.addItem(new Product('Lápis', 1.59));
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDiscount());
+console.log(order.orderStatus);
+order.checkout();
+console.log(order.orderStatus);
---
+++ 17 - TypeScript/ts-solid/src/lsp/main.ts
@@ -0,0 +1,28 @@
+import {
+  FiftyPercentDiscount,
+  NoDiscount,
+  TenPercentDiscount,
+} from './classes/discount';
+import { Order } from './classes/order';
+import { Product } from './classes/product';
+import { ShoppingCart } from './classes/shopping-cart';
+import { Messaging } from './services/messaging';
+import { Persistency } from './services/persistency';
+
+//const fiftyPercentDiscount = new FiftyPercentDiscount();
+// const tenPercentDiscount = new TenPercentDiscount();
+const noDiscount = new NoDiscount();
+const shoppingCart = new ShoppingCart(noDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+const order = new Order(shoppingCart, messaging, persistency);
+
+shoppingCart.addItem(new Product('The Sims 4', 199.9));
+shoppingCart.addItem(new Product('The Sims 5', 239.9));
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDiscount());
+console.log(order.orderStatus);
+order.checkout();
+console.log(order.orderStatus);
